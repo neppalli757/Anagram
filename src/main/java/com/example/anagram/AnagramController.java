5630e5652fb5f2779f92871970fca693a082ff07
neppalli757 <neppalli757@gmail.com>
2017-05-18 11:31:44 -0700

    second commit

--- src/main/java/com/example/anagram/AnagramController.java
+++ src/main/java/com/example/anagram/AnagramController.java
@@ -1,6 +1,8 @@
 package com.example.anagram;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +20,14 @@ public class AnagramController {
     AnagramService anagramService;
 
     @RequestMapping("/word/{word}")
-    public ArrayList<String> getAnagram(@PathVariable("word") String word) {
-        return anagramService.getAnagrams(word);
+    public ResponseEntity<String> getAnagrams(@PathVariable("word") String word) {
+        ArrayList<String> arrayList = anagramService.getAnagrams(word);
+        if( arrayList.size() > 0) {
+            return ResponseEntity.status(HttpStatus.OK).body(arrayList.toString());
+        } else  {
+            String status = "HTTP " + HttpStatus.NOT_FOUND + " Not Found" + System.lineSeparator();
+            String message = "{ 'message' : 'Couldn't find word '"+  word +"}";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(status + message);
+        }
     }
 }
